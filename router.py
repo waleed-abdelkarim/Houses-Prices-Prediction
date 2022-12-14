7fbb360bfb6421c9fd0b59d1e773ea8df4e74e54
waleed-abdelkarim <441106059@student.ksu.edu.sa>
2022-12-14 17:51:57 +0300

    Add Project

---
+++ router.py
@@ -0,0 +1,93 @@
+import numpy as np
+import pandas as pd
+import joblib
+import os
+
+import plotly.express as px
+import plotly
+import json
+
+from flask import Flask, render_template, request
+
+
+from utils import preprocess_new
+
+
+## initiliaze
+app = Flask(__name__)
+
+
+model = joblib.load('model_XGBoost.pkl')
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    FILE_PATH = os.path.join(os.getcwd(), 'housing.csv')
+    df_housing = pd.read_csv(FILE_PATH)
+  
+    fig = px.scatter_mapbox(df_housing, 
+                            lat="latitude", 
+                            lon="longitude", 
+                            hover_data=["population", "ocean_proximity"],
+                            color="median_house_value",
+                            color_continuous_scale=px.colors.sequential.Jet,
+                            size=df_housing['population']/100,
+    
+                            zoom=4)
+
+    fig.update_layout(mapbox_style="open-street-map")
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)  
+    return render_template('index.html', graphJSON=graphJSON)
+
+
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    if request.method == 'POST': 
+        print(11)
+        print('11', request.form.keys)
+        longitude = float(request.form['longitude'])
+        latitude = float(request.form['latitude'])
+        medianAge = float(request.form['medianAge'])
+        totalRooms = float(request.form['totalRooms'])
+        totalbedrooms = float(request.form['bedRooms'])
+
+        population = float(request.form['population'])
+        households = float(request.form['household'])
+        medianIncome = float(request.form['medianIncome'])
+        oceanProximity = request.form['oceanProximity']
+
+        # Concatenate all Inputs
+        X_new = pd.DataFrame({'longitude': [longitude], 'latitude': [latitude], 'housing_median_age': [medianAge], 'total_rooms': [totalRooms],
+                              'total_bedrooms': [totalbedrooms], 'population': [population], 'households': [households], 'median_income': [medianIncome],
+                              'ocean_proximity': [oceanProximity]
+                              })
+
+        # Feature Engineering we did
+        X_new['rooms_per_household'] = X_new['total_rooms'] / X_new['households']
+        X_new['bedrooms_per_rooms'] = X_new['total_bedrooms'] / X_new['total_rooms']
+        X_new['pioulation_per_household'] = X_new['population'] / X_new['households']
+
+
+        # Call the Function and Preprocess the New Instances
+        X_processed = preprocess_new(X_new)
+
+        # call the Model and predict
+        y_pred_new = model.predict(X_processed)
+        y_pred_new = '{:.3f}'.format(y_pred_new[0])
+
+        return render_template('predict.html', pred_val=y_pred_new)
+    else:
+        return render_template('predict.html')
+ 
+
+
+
+
+
+## terminal
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ utils.py
@@ -0,0 +1,72 @@
+
+import pandas as pd
+import os
+
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn_features.transformers import DataFrameSelector
+
+
+# Data preprocessing 
+
+
+# import the dataset file
+FILE_PATH = os.path.join(os.getcwd(), 'housing.csv')
+df_housing = pd.read_csv(FILE_PATH)
+
+# replace the (<1H OCEAN) with (1H OCEAN)
+df_housing['ocean_proximity'].replace('<1H OCEAN', '1H OCEAN', inplace=True)
+
+# Making new features
+df_housing['rooms_per_household'] = df_housing['total_rooms'] / df_housing['households']
+df_housing['bedrooms_per_rooms'] = df_housing['total_bedrooms'] / df_housing['total_rooms']
+df_housing['pioulation_per_household'] = df_housing['population'] / df_housing['households']
+
+
+# X = features, and y = target
+X = df_housing.drop(columns='median_house_value', axis=1)
+y = df_housing['median_house_value']
+
+# split the dataset
+X_train, X_test, y_train, y_test = train_test_split(X, y, shuffle=True, test_size=0.15, random_state=42)
+
+
+
+num_cols = [col for col in X_train.columns if X_train[col].dtype in ['int32', 'int64', 'float32', 'float64']]
+categ_cols = [col for col in X_train.columns if X_train[col].dtype not in ['int32', 'int64', 'float32', 'float64']]
+
+
+num_pipeline = Pipeline(steps=[
+    ('selector', DataFrameSelector(num_cols)),
+    ('imputer', SimpleImputer(strategy='median')),
+    ('scaler', StandardScaler())
+])
+
+categ_pipeline = Pipeline(steps=[
+    ('selector', DataFrameSelector(categ_cols)),
+    ('imputer', SimpleImputer(strategy='constant', fill_value='missing')),
+    ('OHE', OneHotEncoder(sparse=False))
+])
+
+total_pipeline = FeatureUnion(transformer_list=[
+    ('num_pipe', num_pipeline),
+    ('categ_pipe', categ_pipeline)
+])
+
+X_train_final = total_pipeline.fit_transform(X_train)
+
+
+def preprocess_new(X_new):
+    '''
+    This function will process new instance before predicte using model
+    
+    Atgs:
+        X_new: The new features to make data preprocessing on them
+    
+    Returns:
+        Preprocessed features ready to enter the model
+    '''
+    return total_pipeline.transform(X_new)
+
